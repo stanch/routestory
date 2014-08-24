98170d62714a6753d5e3527507c11506f69e9141
Nikolay Stanchenko <nick.stanch@gmail.com>
2014-08-24 22:17:26 +0400

    Remove control panel

--- src/main/scala/net/routestory/recording/RecordActivity.scala
+++ src/main/scala/net/routestory/recording/RecordActivity.scala
@@ -1,44 +1,25 @@
 package net.routestory.recording
 
-import akka.actor.{ ActorSystem, ActorRef }
-import akka.pattern.ask
-import akka.util.Timeout
-import android.app.{ NotificationManager, PendingIntent }
-import android.content.{ ComponentName, ServiceConnection, Context, Intent }
-import android.location.{ Location ⇒ AndroidLocation }
-import android.os.{ IBinder, Bundle }
-import android.support.v4.app.{ Fragment, TaskStackBuilder, NotificationCompat }
+import akka.actor.ActorSystem
+import android.content.{ ComponentName, Context, Intent, ServiceConnection }
+import android.os.{ Bundle, IBinder }
+import android.support.v4.app.Fragment
 import android.support.v4.view.ViewPager
-import android.view.{ KeyEvent, Menu, MenuItem }
+import android.view.{ Menu, MenuItem }
 import android.widget.ProgressBar
-import com.google.android.gms.common._
-import com.google.android.gms.location.{ LocationClient, LocationListener, LocationRequest }
 import macroid.FullDsl._
-import macroid.akkafragments.AkkaActivity
+import macroid.{ IdGeneration, Ui }
 import macroid.contrib.Layouts.VerticalLinearLayout
-import macroid.Ui
 import macroid.contrib.PagerTweaks
-import macroid.{ IdGeneration, Tweak }
 import net.routestory.R
-import net.routestory.browsing.story.DisplayActivity
-import net.routestory.data.Story
 import net.routestory.editing.EditActivity
-import net.routestory.recording.logged.{ ControlPanelFragment, Dictaphone }
 import net.routestory.recording.manual.AddMediaFragment
-import net.routestory.recording.suggest.{ Suggester, SuggestionsFragment }
+import net.routestory.recording.suggest.SuggestionsFragment
 import net.routestory.ui.{ FragmentPaging, RouteStoryActivity }
-import net.routestory.util.Implicits._
 import net.routestory.util.Shortuuid
-import play.api.data.mapping.{ From, To }
-import play.api.libs.json.{ JsObject, JsValue, Json }
-import resolvable.Resolvable
 
-import scala.async.Async._
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration._
-import scala.concurrent.{ Await, Promise }
-import scala.util.Try
-import scala.util.control.NonFatal
+import scala.concurrent.Promise
 
 trait RecordFragment { self: Fragment ⇒
   lazy val actorSystem = getActivity.asInstanceOf[RecordActivity].actorSystem.future
@@ -60,8 +41,7 @@ class RecordActivity extends RouteStoryActivity with IdGeneration with FragmentP
         getTabs(
           "Add stuff" → f[AddMediaFragment].factory,
           "Map" → f[CartographyFragment].factory,
-          "Suggestions" → f[SuggestionsFragment].factory,
-          "Control panel" → f[ControlPanelFragment].factory
+          "Suggestions" → f[SuggestionsFragment].factory
         )
       )
     })
@@ -117,11 +97,6 @@ class RecordActivity extends RouteStoryActivity with IdGeneration with FragmentP
     case R.id.addStuff ⇒
       runUi {
         pager <~ PagerTweaks.page(0, smoothScroll = true)
-      }
-      true
-    case R.id.controlPanel ⇒
-      runUi {
-        pager <~ PagerTweaks.page(3, smoothScroll = true)
       }
       true
     case R.id.finish ⇒
